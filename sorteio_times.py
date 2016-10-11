cf4373f667777740a5c1295294403f8895249cd6
dguirra <diegoguirra23@gmail.com>
2016-10-11 14:25:29 -0300

    Mais times e lista de Players

--- sorteio_times.py
+++ sorteio_times.py
@@ -1,6 +1,8 @@
 import random
 
-times = ['Atletico de Madri', 'Benfica', 'Spurs', 'Borussia']
+times = ['Benfica', 'Tottenham', 'Borussia Dortmund', 'Bayer Leverkusen', 'Porto', 'Leicester',]
+players = ['Guirra', 'Gustavo', 'Bertolini', 'Fernando', 'Leonardo',
+           'Gel', 'Daniel', 'Lirio', 'Felipe', 'Gabriel']
 
 print random.shuffle(times)
 print times
