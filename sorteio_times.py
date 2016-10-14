68f743f04639a514aa3c2906a9bedd1c951cb04c
dguirra <diego.guirra@titansgroup.com.br>
2016-10-14 18:45:32 -0300

    Updates

--- sorteio_times.py
+++ sorteio_times.py
@@ -1,8 +1,8 @@
 import random
 
 nomes = ['Gel', 'Daniel', 'Lirio', 'Felipe', 'Gabriel', 'Gustavo',
-         'Bertolini', 'Guirra', 'Leornardo', 'Fernando']
-times = ['Barcelona', 'Real Madrid', 'Borussia Dortmund', 'Bayer Munique',
+         'Bertolini', 'Guirra', 'Leonardo', 'Fernando']
+times = ['Barcelona', 'Real Madrid', 'Borussia Dortmund', 'Bayern Munique',
          'Chelsea', 'PSG', 'Atl Madrid', 'Manchester City', 'Manchester United',
          'Juventus', 'Tottenham', 'Liverpool']
 for i, x in enumerate(nomes):
