b006a91def79337dca79dc7615be2405cc7f256c
dguirra <diego.guirra@titansgroup.com.br>
2016-10-24 16:51:11 -0200

    Incluso input para qtd de jogadores e raw_input para nomes dos jogadores

--- sorteio_times2.py
+++ sorteio_times2.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 import random
+
+
 nomes = []
-qtd = int(input("Digite o número de jogadores: "))
-cont = 1
-while cont <= qtd:
-    n = [str(x) for x in input("Digite o nome do player : ").split()]
+validate = False
+
+while validate is False:
+    qtd = int(input("Digite o número de jogadores: "))
+    if qtd >= 2 and qtd <= 12:
+        validate = True
+    continue
+    print "Você deve inserir a qtd de jogadores com no mínimo 2 e no máximo 12"
+
+for i in range(qtd):
+    n = raw_input("Digite o nome do player : ")
     nomes.append(n)
-    cont = cont + 1
+
 times = ['Barcelona', 'Real Madrid', 'Borussia Dortmund', 'Bayern Munique',
          'Chelsea', 'PSG', 'Atl Madrid', 'Manchester City', 'Manchester United',
          'Juventus', 'Tottenham', 'Liverpool']
+
 for i, x in enumerate(nomes):
     choice = random.choice(times)
-    print "{} joga com o time {}".format(x, choice)
+    print ("{} joga com o time {}".format(x, choice))
     times.remove(choice)
